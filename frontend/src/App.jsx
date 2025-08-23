86cac3bbcb41dd585fba53f8410b5c0b6721d00b
yohannes <yohannestagel21@gmail.com>
2025-08-23 21:55:51 +0300

    this is some side bar color test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Saidbar from './componet/saidbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
    <>
-    hellow
+    <Saidbar/>
    </>
   )
 }
---
+++ frontend/src/componet/Saidbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Saidbar() {
+  return (
+   <div className="bg-green-500 w-2/5 p-10 h-screen">
+   <h1 className="text-red-600 size-5">side bare</h1>
+
+   </div>
+  )
+}
+
+export default  Saidbar
