d4abb3116524705c26794994ef89e9c2a984834c
yohannes <yohannestagel21@gmail.com>
2025-08-25 16:58:00 +0300

    test

--- frontend/src/componet/Saidbar.jsx
+++ frontend/src/componet/Saidbar.jsx
@@ -4,6 +4,8 @@ function Saidbar() {
   return (
    <div className="bg-green-500 w-2/5 p-10 h-screen">
    <h1 className="text-red-600 size-5">side bare</h1>
+   <h1 className="text-red-600 size-5">side bare</h1>
+
 
    </div>
   )
